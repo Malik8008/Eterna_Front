6cc7c49e6cefeeb4071f1136fa3288dce1c29fff
Malik8008 <tu60prbs1@code.edu.az>
2022-06-13 06:54:19 +0400

    Home Front complete

---
+++ assets/JS/style.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:50,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
